a034574c381cdff417a0678fef4b2f7c3b5a8489
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-22 20:13:49 +0100

    feat: Implemeneted insertion sort using InsertionSort.py module

--- app.py
+++ app.py
@@ -1,36 +1,26 @@
-import time
 from src.python.algDs import insertionSort
-
-
-def timeIt(func):
-    """
-    Compute and display the time of execution in milliseconds of a function
-    
-    Args:
-        func: function to time
-    """
-    start = time.time()
-    func()
-    end = time.time()
-    exe_time = (end - start) * 10**3
-    print(f"The time of execution is: {exe_time} ms")
 
 
 def display_menu():
     """
     Display the menu for the user to select the type of sort to perform
     """
-    print("\nSelect the type of sort to perform:")
-    print("1. Sort an array")
-    print("2. Search in an array")
-    print("3. Exit")
+    option_text = "\n#Select the type of process to perform:\n"
+    print(f"\n{'-' * len(option_text)}")
+    print(option_text)
+    print("1. Create an array")
+    print("2. Sort an array")
+    print("3. Search in an array")
+    print("4. Exit")
 
 
-def display_sort():
+def display_sort_options():
     """
     Display the menu for the user to select the type of sort to perform
     """
-    print("\nSelect the type of sort to perform:")
+    option_text = "\n#Select the type of sort to perform:\n"
+    print(f"\n{'-' * len(option_text)}")
+    print(option_text)
     print("1. Insertion sort")
     # print("2. Selection sort")
     # print("3. Bubble sort")
@@ -39,11 +29,13 @@ def display_sort():
     # print("6. Heap sort")
     print("7. Exit")
 
-def display_search():
+def display_search_options():
     """
     Display the menu for the user to select the type of search to perform
     """
-    print("\nSelect the type of search to perform:")
+    option_text = "\n#Select the type of search to perform:\n"
+    print(f"\n{'-' * len(option_text)}")
+    print(option_text)
     print("1. Linear search")
     print("2. Binary search")
     print("3. Exit")
@@ -60,24 +52,32 @@ def invalid_option(num_options):
     print('-' * len(invalid_option_text))
 
 
-def array_sort(arr):
+def create_array():
+    """
+    Create an array to sort or search in
+    """
+    pass
+
+
+def sort_array(arr):
     """
     Perform the sort operation based on the user's selection
 
     Args:
         arr: list of integers to sort
     """
-    display_sort()
-    sort_option = int(input("Enter your choice (1-7): "))
+    display_sort_options()
+    sort_option = int(input("\n#Enter your choice (1-7):~$ "))
 
     while True:
         if sort_option == 1:
-            timeIt(insertionSort.insertionSort())
+            insertionSort.insertionSort(arr=arr)
+            break
         elif sort_option == 7:
             """
             Exits the application.
             """
-            print("Exiting the application!")
+            print("\nExiting the sort!")
             break
         else:
             invalid_option(num_options=7)
@@ -88,21 +88,25 @@ if __name__ == "__main__":
     Main function to run the program 
     and display the menu for the user to select the type of sort or search to perform
     """
-    arr = [5, 2, 4, 6, 1, 3]
+    arr = [5, 2, 4, 9, 1, 3]
+    """""
+    pre arry of a cutom array
+    """
     soret_selected = False
     search_selected = False
 
     while True:
         display_menu()
-        option = int(input("Enter your choice (1-3): "))
+        option = int(input("\n#Enter your choice (1-3):~$ "))
 
         if option == 1:
-            # array_sort(arr)
+            # arr = create_array()
             break
         elif option == 2:
-            break
-
+            sort_array(arr)
         elif option == 3:
+            break
+        elif option == 4:
             """
             Exits the application.
             """
