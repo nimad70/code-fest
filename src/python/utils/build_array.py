a92818be7ee216c96c51969b677ada1c9f23e9a7
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 17:25:36 +0100

    feat: Validated the input value for option whether it is a digit or not.

--- src/python/utils/build_array.py
+++ src/python/utils/build_array.py
@@ -12,61 +12,66 @@ import src.python.utils.validate as validate
 def create_array():
     """
     Create an array to sort or search in
-    
+
     Returns:
         arr: list of integers
     """
     while True:
-        display_creat_array_options()
-        array_option = int(input("\n#Enter your choice (1-3):~$ "))
+        try:
+            display_creat_array_options()
+            array_option = int(input("\n#Enter your choice (1-3):~$ "))
 
-        if array_option == 1:
-            while True:
-                try:
-                    num = int(input("\n#Enter the number of elements in the array (less than 100):~$ "))
-                    if len(str(num)) <= 2 :
-                        arr = []
-                        for i in range(num):
-                            while True:
-                                try:
-                                    array_input = int(input(f"\n#Enter the element {i+1} ( <= 5 digits): "))
-                                    if len(str(array_input)) <= 5:
-                                        arr.append(array_input)
-                                        break
-                                    else:
+            if array_option == 1:
+                while True:
+                    try:
+                        num = int(input("\n#Enter the number of elements in the array (less than 100):~$ "))
+                        if len(str(num)) <= 2 :
+                            arr = []
+                            for i in range(num):
+                                while True:
+                                    try:
+                                        array_input = int(input(f"\n#Enter the element {i+1} ( <= 5 digits): "))
+                                        if len(str(array_input)) <= 5:
+                                            arr.append(array_input)
+                                            break
+                                        else:
+                                            print("\n=> Invalid input. Please enter a valid number.")
+                                    except:
                                         print("\n=> Invalid input. Please enter a valid number.")
-                                except:
-                                    print("\n=> Invalid input. Please enter a valid number.")
-                        print(f"\n => The array: {arr}")
-                        return arr
-                    else:
-                        print("\n=> Really? more than <100> inputs?! Please enter a valid number.")
-                except ValueError:
-                    print("\n=> Invalid input. Please enter a valid number.")
+                            print(f"\n => The array: {arr}")
+                            return arr
+                        else:
+                            print("\n=> Really? more than <100> inputs?! Please enter a valid number.")
+                    except ValueError:
+                        print("\n=> Invalid input. Please enter a valid number.")
+            
+            elif array_option == 2:
+                while True:
+                    try:
+                        num = int(input("\n#Enter the number of elements in the array (less than 100):~$ "))
+                        if len(str(num)) <= 2:
+                            arr = random.choices(range(0, 99), k=num)
+                            print(f"\n=> The array: {arr}")
+                            return arr
+                        else:
+                            print("\n=> Really? more than <100> inputs?! Please enter a valid number.")
+                    except ValueError:
+                        print("\n=> Invalid input. Please enter a valid number.")
+                        
+            elif array_option == 3:
+                """
+                Exits the build an array.
+                """
+                print("\nContinue with the default array!")
+                print("-> The default array: [5, 2, 4, 6, 1, 3]")
+                break
+            
+            else:
+                validate.invalid_option(num_options=3)
         
-        elif array_option == 2:
-            while True:
-                try:
-                    num = int(input("\n#Enter the number of elements in the array (less than 100):~$ "))
-                    if len(str(num)) <= 2:
-                        arr = random.choices(range(0, 99), k=num)
-                        print(f"\n=> The array: {arr}")
-                        return arr
-                    else:
-                        print("\n=> Really? more than <100> inputs?! Please enter a valid number.")
-                except ValueError:
-                    print("\n=> Invalid input. Please enter a valid number.")
-                    
-        elif array_option == 3:
-            """
-            Exits the build an array.
-            """
-            print("\nContinue with the default array!")
-            print("-> The default array: [5, 2, 4, 6, 1, 3]")
-            break
-        
-        else:
-            validate.invalid_option(num_options=3)
+        except ValueError:
+            print("\n=> Invalid input. Please enter a valid number.")
+
 
 
 if __name__ == "__main__":
