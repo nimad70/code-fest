bd1f08e257b324bf97b91547a36c3a81f04d7ef6
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 12:16:59 +0100

    feat: Imported required modules from utils directory. Refactored the code.

--- app.py
+++ app.py
@@ -1,62 +1,7 @@
 from src.python.algDs import insertionSort
-
-
-def display_menu():
-    """
-    Display the menu for the user to select the type of sort to perform
-    """
-    option_text = "\n#Select the type of process to perform:\n"
-    print(f"\n{'-' * len(option_text)}")
-    print(option_text)
-    print("1. Create an array")
-    print("2. Sort an array")
-    print("3. Search in an array")
-    print("4. Exit")
-
-
-def display_sort_options():
-    """
-    Display the menu for the user to select the type of sort to perform
-    """
-    option_text = "\n#Select the type of sort to perform:\n"
-    print(f"\n{'-' * len(option_text)}")
-    print(option_text)
-    print("1. Insertion sort")
-    # print("2. Selection sort")
-    # print("3. Bubble sort")
-    # print("4. Quick sort")
-    # print("5. Merge sort")
-    # print("6. Heap sort")
-    print("7. Exit")
-
-def display_search_options():
-    """
-    Display the menu for the user to select the type of search to perform
-    """
-    option_text = "\n#Select the type of search to perform:\n"
-    print(f"\n{'-' * len(option_text)}")
-    print(option_text)
-    print("1. Linear search")
-    print("2. Binary search")
-    print("3. Exit")
-
-
-def invalid_option(num_options):
-    """
-    Handles invalid menu selections.
-    """
-    invalid_option_text = f"Invalid choice. Please select a valid option between 1 to {num_options}."
-    print()
-    print(f"\n{'-' * len(invalid_option_text)}")
-    print(f"{invalid_option_text}")
-    print('-' * len(invalid_option_text))
-
-
-def create_array():
-    """
-    Create an array to sort or search in
-    """
-    pass
+from src.python.utils import validate
+from src.python.utils import build_array
+from src.python.utils import menu
 
 
 def sort_array(arr):
@@ -66,7 +11,7 @@ def sort_array(arr):
     Args:
         arr: list of integers to sort
     """
-    display_sort_options()
+    menu.display_sort_options()
     sort_option = int(input("\n#Enter your choice (1-7):~$ "))
 
     while True:
@@ -80,7 +25,7 @@ def sort_array(arr):
             print("\nExiting the sort!")
             break
         else:
-            invalid_option(num_options=7)
+            validate.invalid_option(num_options=7)
 
 
 if __name__ == "__main__":
@@ -96,12 +41,11 @@ if __name__ == "__main__":
     search_selected = False
 
     while True:
-        display_menu()
+        menu.display_menu()
         option = int(input("\n#Enter your choice (1-3):~$ "))
 
         if option == 1:
-            # arr = create_array()
-            break
+            arr = build_array.create_array()
         elif option == 2:
             sort_array(arr)
         elif option == 3:
@@ -114,4 +58,4 @@ if __name__ == "__main__":
             break
         
         else:
-            invalid_option(num_options=3)
+            validate.invalid_option(num_options=3)
