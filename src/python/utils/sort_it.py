279ed3c122ce5c935d7b54699612d23ccdfe79a6
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 17:39:20 +0100

    feat: Seperated selection of varios sort to apply on array from app.p to a seperate module called sort_it.py

---
+++ src/python/utils/sort_it.py
@@ -0,0 +1,30 @@
+"""
+This module contains the sort_array function which performs the sort operation based on the user's selection.
+"""
+from src.python.algDs import insertionSort
+from src.python.utils import menu
+
+
+def sort_array(arr):
+    """
+    Perform the sort operation based on the user's selection
+
+    Args:
+        arr: list of integers to sort
+    """
+
+    while True:
+        menu.display_sort_options()
+        sort_option = int(input("\n#Enter your choice (1-7):~$ "))
+
+        if sort_option == 1:
+            insertionSort.insertionSort(arr=arr)
+            break
+        elif sort_option == 7:
+            """
+            Exits the sort.
+            """
+            print("\nExiting the sort!")
+            break
+        else:
+            validate.invalid_option(num_options=7)
