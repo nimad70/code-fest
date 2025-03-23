f1e0c7d80b5a19170cf1e05e9fec579020de9e0d
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 17:14:33 +0100

    feat: Implemenetd is_valid_number() in validate.py to check if the input is all digits

--- src/python/utils/validate.py
+++ src/python/utils/validate.py
@@ -12,11 +12,23 @@ def invalid_option(num_options):
     print('-' * len(invalid_option_text))
 
 
-def is_valid_number(num):
+def is_valid_number(is_list, num):
     """
-    Check if the input is a valid single number
+    Check if the input is a valid number
     """
-    num_reg = re.compile('^[0-9]$')
-    matched = bool(num_reg.fullmatch(num))
+    list_num_reg = re.compile('^[0-9]{1,5}$')
+    num_reg = re.compile('^[0-9]{1,5}$')
 
+    if is_list:
+        matched = list_num_reg.fullmatch(num)
+        return matched
+    
+    matched = bool(num_reg.fullmatch(num))
     return matched
+
+
+if __name__ == "__main__":
+    num = input("\n#Enter the number of elements in the array:~$ ")
+    is_valid = is_valid_number(is_list=True, num=num)
+    if is_valid:
+        print("\n#is valid")
