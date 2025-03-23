634de33c58ba706344fc4ede1dfdfa3b3dc8c711
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 12:13:55 +0100

    feat: Implemenetd menu.py module to display menus for various processes

---
+++ src/python/utils/menu.py
@@ -0,0 +1,38 @@
+def display_menu():
+    """
+    Display the menu for the user to select the type of sort to perform
+    """
+    option_text = "\n#Select the type of process to perform:\n"
+    print(f"\n{'-' * len(option_text)}")
+    print(option_text)
+    print("1. Create an array")
+    print("2. Sort an array")
+    print("3. Search in an array")
+    print("4. Exit")
+
+
+def display_sort_options():
+    """
+    Display the menu for the user to select the type of sort to perform
+    """
+    option_text = "\n#Select the type of sort to perform:\n"
+    print(f"\n{'-' * len(option_text)}")
+    print(option_text)
+    print("1. Insertion sort")
+    # print("2. Selection sort")
+    # print("3. Bubble sort")
+    # print("4. Quick sort")
+    # print("5. Merge sort")
+    # print("6. Heap sort")
+    print("7. Exit")
+
+def display_search_options():
+    """
+    Display the menu for the user to select the type of search to perform
+    """
+    option_text = "\n#Select the type of search to perform:\n"
+    print(f"\n{'-' * len(option_text)}")
+    print(option_text)
+    print("1. Linear search")
+    print("2. Binary search")
+    print("3. Exit")
