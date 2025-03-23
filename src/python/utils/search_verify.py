d009f5516eb95317299c5ab71dfb6b07b3c6e7fa
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 19:24:49 +0100

    feat: Initiliazed and moved verify() from linearSearch.py module to use the verification in other searhc modules.

---
+++ src/python/utils/search_verify.py
@@ -0,0 +1,9 @@
+def verify(index):
+    """
+    Verify the index position of the target in the array
+    """
+    if index is not None:
+        print("Target found at index position:", index)
+    else:
+        print("Traget not found in the array")
+
