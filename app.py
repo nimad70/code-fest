8261ece279b79db8c4b12abf5e4b9bdbc3a1bb0e
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 17:41:18 +0100

    reafactor: Moved sort section to a sort_it.py and declared docstrings in app.py

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-from src.python.algDs import insertionSort
+"""
+This is the main file that runs the program.
+"""
 from src.python.utils import validate
 from src.python.utils import build_array
 from src.python.utils import menu
-
-
-def sort_array(arr):
-    """
-    Perform the sort operation based on the user's selection
-
-    Args:
-        arr: list of integers to sort
-    """
-
-    while True:
-        menu.display_sort_options()
-        sort_option = int(input("\n#Enter your choice (1-7):~$ "))
-
-        if sort_option == 1:
-            insertionSort.insertionSort(arr=arr)
-            break
-        elif sort_option == 7:
-            """
-            Exits the sort.
-            """
-            print("\nExiting the sort!")
-            break
-        else:
-            validate.invalid_option(num_options=7)
+from src.python.utils import sort_it
 
 
 if __name__ == "__main__":
@@ -46,7 +24,7 @@ if __name__ == "__main__":
             if option == 1:
                 arr = build_array.create_array()
             elif option == 2:
-                sort_array(arr)
+                sort_it.sort_array(arr)
             elif option == 3:
                 break
             elif option == 4:
