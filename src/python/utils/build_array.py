4e1dd3b803ffbd5e61b19ae552d92a0483f5642f
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-23 16:26:27 +0100

    feat: Implemented building a customized array from taking inputs from users in build_array.py

--- src/python/utils/build_array.py
+++ src/python/utils/build_array.py
@@ -3,55 +3,69 @@ This module contains the function to create an array to sort or search in.
 """
 
 import random
-from src.python.utils.menu import display_creat_array_options
-from src.python.utils import validate
+# from src.python.utils.menu import display_creat_array_options
+# import src.python.utils.validate as validate 
+import menu
+import validate
 
 
 def create_array():
     """
     Create an array to sort or search in
     """
-    # is_valid = False
-    # while not is_valid:
-    #     num = int(input("\n#Enter the number of elements in the array:~$ "))
-    #     if invalid_number.is_valid(num=num):
-    #         arr = []
-    #         for i in range(num):
-    #             is_element_invalid = False
-    #             while not is_element_invalid:
-    #                 arr_inputs = int(input(f"\n#Enter the element {i+1}: "))
-    #                 if invalid_number.is_valid(num=arr_inputs):
-    #                     arr.append(arr_inputs)
-    #                     is_element_invalid = False
-    #                 else:
-    #                     print("\nInvalid input. Please enter a valid number.")
-
-    #         is_valid = False
-    #         return arr
-    #     else:
-    #         print("\nInvalid input. Please enter a valid number.")
     while True:
-        display_creat_array_options()
+        menu.display_creat_array_options()
         array_option = int(input("\n#Enter your choice (1-3):~$ "))
-        
+
         if array_option == 1:
-            num = int(input("\n#Enter the number of elements in the array:~$ "))
-            arr = []
-            for i in range(num):
-                arr_inputs = int(input(f" -> Enter the element {i+1}: "))
-                arr.append(arr_inputs)
-            print(f"\n The array: {arr}")
-            return arr
+            # num = int(input("\n#Enter the number of elements in the array:~$ "))
+            # arr = []
+            # for i in range(num):
+            #     arr_inputs = int(input(f" -> Enter the element {i+1}: "))
+            #     arr.append(arr_inputs)
+
+            # is_valid = False
+            while True:
+                try:
+                    num = int(input("\n#Enter the number of elements in the array (less than 100):~$ "))
+                    if len(str(num)) <= 2 :
+                        arr = []
+                        for i in range(num):
+                            while True:
+                                try:
+                                    array_input = int(input(f"\n#Enter the element {i+1} ( <= 5 digits): "))
+                                    if len(str(array_input)) <= 5:
+                                        arr.append(array_input)
+                                        break
+                                    else:
+                                        print("\n=> Invalid input. Please enter a valid number.")
+                                except:
+                                    print("\n=> Invalid input. Please enter a valid number.")
+                    else:
+                        print("\n=> Really? more than <100> inputs?! Please enter a valid number.")
+                except ValueError:
+                    print("\n=> Invalid input. Please enter a valid number.")
+                finally:
+                    print(f"\n The array: {arr}")
+                    return arr
+        
         elif array_option == 2:
             num = int(input("\n#Enter the number of elements in the array:~$ "))
             arr = random.choices(range(0, 99), k=num)
             print(f"\n The array: {arr}")
             return arr
+        
         elif array_option == 3:
             """
-            Exits the application.
+            Exits the build an array.
             """
-            print("\nExiting the sort!")
+            print("\nContinue with the default array!")
+            print("-> The default array: [5, 2, 4, 6, 1, 3]")
             break
+        
         else:
             validate.invalid_option(num_options=3)
+
+
+if __name__ == "__main__":
+    create_array()
