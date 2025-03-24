eb43cffe0dcd32a2a627e9b01b09cbed1cc69e23
Nima Daryabar <nima.daryabar@gmail.com>
2025-03-24 14:41:21 +0100

    feat: Implemented binary search using two different procedure, taking floor and ceil divisions in binarySearch.py

--- src/python/algDs/binarySearch.py
+++ src/python/algDs/binarySearch.py
@@ -5,19 +5,84 @@ is eliminated and the search continues on the remaining half, again taking the m
 The time complexity of the binary search algorithm is O(log n) where n is the number of elements in the array.
 """
 import time
-from src.python.utils.search_verify import verify
+# from src.python.utils.search_verify import verify
 
+"""
+Verify whether the target is in the array
+"""
 
-def binarySearch(arr, key):
-    pass
-
-
-def binarySearchTimeCmpx(arr, key):
+def verify(index):
     """
-    Perform a linear search on an array of integers and return the execution time.
+    Verify the index position of the target in the array
+    """
+    if index is not None:
+        print("\n$:) => Hooray! Target found at index position:", index)
+    else:
+        print("\n$:( => Traget not found in the array")
+
+def ceil(l, r, divident=2):
+    """
+    Return the ceiling value of a number
+    """
+    return int(-1 * ((-1 * (l+r)) // divident))
+
+
+def floor(l, r, divident=2):
+    """
+    Return the floor value of a number
+    """
+    return int((l+r) // divident)
+
+
+def binarySearchFloor(arr, key):
+    l = 0
+    r = len(arr) - 1 
+    
+    while l <= r:
+        mid = floor(l, r)
+        if key == arr[mid]:
+            return mid
+        elif key < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
+    
+    return None
+
+
+def binarySearchCeil(arr, key):
+    l = 0
+    r = len(arr) - 1
+    
+    while l != r:
+        mid = ceil(l, r)
+        if key < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid
+    if arr[l] == key:
+        return l
+    
+    return None
+
+def binarySearchFloorTimeCmpx(arr, key):
+    """
+    Perform a linear search by taking the floor of ((l+r)/2) on an array of integers and return the execution time.
     """
     start = time.time()
-    index = binarySearch(arr=arr, key=key)
+    index = binarySearchFloor(arr=arr, key=key)
+    end = time.time()
+    execution_time = end - start
+
+    return index, execution_time
+
+
+def binarySearchCeilTimeCmpx(arr, key):
+    """
+    Perform a linear search by taking the ceiling of ((l+r)/2) on an array of integers and return the execution time.
+    """
+    start = time.time()
+    index = binarySearchCeil(arr=arr, key=key)
     end = time.time()
     execution_time = end - start
 
@@ -28,16 +93,23 @@ def display_binarySearch(arr, key):
     """
     Display the result of the binary search operation
     """
-    index, execution_time = binarySearchTimeCmpx(arr=arr, key=key)
+    print(f"\n=> The array: {arr}")
+    print(f"=> The target key: {key}")
+
+    index, execution_time_floor = binarySearchFloorTimeCmpx(arr=arr, key=key)
     verify(index)
-    print(f"=> Execution time: {execution_time*10**3:.3f} seconds")
+    print(f"=> Execution time by taking the floor of ((l+r)/2): {execution_time_floor*10**3:.3f} seconds")
+
+    index, execution_time_ceil = binarySearchFloorTimeCmpx(arr=arr, key=key)
+    verify(index)
+    print(f"=> Execution time by taking the ceiling of ((l+r)/2): {execution_time_ceil*10**3:.3f} seconds")
 
 
 if __name__ == "__main__":
     arr = [i for i in range(1, 11)]
-    
-    result = binarySearch(arr, key=5)
-    verify(result)
+    print(arr)
 
-    result = binarySearch(arr, key=12)
-    verify(result)
+    display_binarySearch(arr, key=3)
+    display_binarySearch(arr, key=5)
+    display_binarySearch(arr, key=7)
+    display_binarySearch(arr, key=12)
